3d376aa52502eb286c09781ea3d878b71f85ee79
danjberes <68877097+danjberes@users.noreply.github.com>
2021-02-21 22:24:42 -0600

    Update logic.js

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -27,7 +27,7 @@ let map = L.map('mapid', {
 L.control.layers(baseMaps).addTo(map);
 
 //Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/jacodetunde/Mapping_Earthquakes/main/majorAirports.json";
+let airportData = "https://raw.githubusercontent.com/danjberes/Mapping_Earthquakes/main/majorAirports.json";
 
 // Grabbing our GeoJSON data.
 d3.json(airportData).then(function(data){
